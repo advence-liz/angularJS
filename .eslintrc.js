52c45dd8a3c2b053a9950e85f5385e9940143720
advence-liz <1012515183@qq.com>
2016-09-28 09:08:13 +0800

    添加.gitignore .eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+     //   "indent": [
+     //       "warn",
+     //       4
+     //   ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
