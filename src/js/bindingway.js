0612645f223607027daa49e783d5562f2a5bd4d8
advence-liz <1012515183@qq.com>
2016-10-13 20:08:34 +0800

    add bindingway

--- src/js/bindingway.js
+++ src/js/bindingway.js
@@ -5,8 +5,14 @@
         mainApp.controller("mainController", ['$scope','$controller',function Controller($scope) {
                   $scope.subject="angular";
                   $scope.author="liz";
-                  var vm=$scope.vm;
-               
+                  $scope.vm={};
+                 var vm=$scope.vm;
+                 vm.apple=3;
+                 vm.pen=4;
+                 vm.sum=function(){
+                   return vm.pen+vm.apple;
+                 };
+                 
           
         }]);
 }(window,window.angular);
